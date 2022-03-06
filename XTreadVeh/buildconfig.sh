7c5debd16b372cea69176e15a4803a1a45cfcca6
Gustavo Ramos Rehermann <rehermann6046@gmail.com>
2022-03-05 21:53:17 -0300

    Apply ModBuild update

--- XTreadVeh/buildconfig.sh
+++ XTreadVeh/buildconfig.sh
@@ -9,6 +9,7 @@ export build=20220901
 export version=12
 export debug=1
 export makeint=0
+export incl_readme=0
 
 # It is most likely unnecessary to edit the below defaults. The scripts
 # in the build/ subdirectory provide a sufficiently predictable build environment.
--- XVehicles/buildconfig.sh
+++ XVehicles/buildconfig.sh
@@ -3,11 +3,13 @@
 # Edit this file if necessary; it is read by _build.sh in order to
 # provide some context variables.
 
-export name="XVehicles"
+export name="XVehicles Core"
 export package=XVehicles
 export build=20220901
 export version=12
 export debug=1
+export makeint=0
+export incl_readme=0
 
 # It is most likely unnecessary to edit the below defaults. The scripts
 # in the build/ subdirectory provide a sufficiently predictable build environment.
